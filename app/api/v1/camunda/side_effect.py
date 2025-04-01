bcbfbdd8baa012bb7945c4f1cb9cf13b2e35fd7d
Rafael Nunes <rafael.nunes@bhub.ai>
2025-04-01 16:28:07 -0300

    Added pre-commit to dev dependencies; use uvicorn for local docker dev

--- app/api/v1/camunda/side_effect.py
+++ app/api/v1/camunda/side_effect.py
@@ -1,13 +1,12 @@
 import logging
 
-
+from api.base.endpoints import BaseEndpoint
+from db.session import get_session
 from fastapi import Depends
 from fastapi.responses import JSONResponse
+from schemas.camunda_schema import Event
 from sqlmodel import Session
 
-from api.base.endpoints import BaseEndpoint
-from schemas.camunda_schema import Event
-from db.session import get_session
 
 logger = logging.getLogger(__name__)
 
@@ -22,9 +21,9 @@ class SideEffectEndpoint(BaseEndpoint):
     def __init__(self):
         super().__init__(tags=["side_effect"], prefix=ROUTE_PREFIX)
 
-        @self.router.get(LOG_EVENT_ROUTE)
+        @self.router.post(LOG_EVENT_ROUTE)
         async def log_event(event: Event | None = None, db: Session = Depends(get_session)):
             """Log event"""
-            logger.info(f"Logging event: {event or "Empty event"}")
+            logger.info(f"Logging event: {event or 'Empty event'}")
 
             return JSONResponse(content={"message": "Event logged"})
