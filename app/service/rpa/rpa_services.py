0874700ba80e9826f5d5de0840ca8849e83b022e
Rafael Nunes <rafael.nunes@bhub.ai>
2025-04-30 12:26:45 -0300

    Autenticando requests no Camunda; tratamento de erros no webhook

--- app/service/rpa/rpa_services.py
+++ app/service/rpa/rpa_services.py
@@ -49,6 +49,23 @@ def start_melius_rpa(process_data: dict, db_session: DBSession):
         raise RPAException(str(e))
 
 
+def _make_camunda_request(url, params: dict):
+    headers = {
+        "Content-Type": "application/json",
+    }
+
+    if settings.ENV == "prod":
+        headers["X-API-Key"] = f"{settings.CAMUNDA_API_TOKEN}"
+    else:
+        headers["Authorization"] = f"Basic {settings.CAMUNDA_USERNAME}:{settings.CAMUNDA_PASSWORD}"
+
+    response = httpx.post(url, json=params, headers=headers)
+
+    response.raise_for_status()
+
+    return response
+
+
 def handle_webhook_request(request: MeliusWebhookRequest, db_session: DBSession):
     """
     Webhook para receber update dos RPAs da Melius.
@@ -59,9 +76,9 @@ def handle_webhook_request(request: MeliusWebhookRequest, db_session: DBSession)
     """
     stmt = select(RPAEventLog).where(
         RPAEventLog.process_id == request.id_tarefa_cliente,
-        RPAEventLog.event_data.op("->>")("tokenRetorno") == request.token_retorno,
+        RPAEventLog.event_data.op("->>")("tokenRetorno") == request.token_retorno,  # type: ignore
     )
-    rpa_event_logs = db_session.execute(stmt).scalars().all()
+    rpa_event_logs = db_session.exec(stmt).scalars().all()
 
     if len(rpa_event_logs) != 1 or rpa_event_logs[0].event_type != RPAEventTypes.START:
         raise RPAException("Token inválido ou tarefa não encontrada")
@@ -79,26 +96,29 @@ def handle_webhook_request(request: MeliusWebhookRequest, db_session: DBSession)
                         "url": arquivo.url,
                         "nomeArquivo": arquivo.nome_arquivo,
                     }
-                    for arquivo in request.arquivos_gerados
+                    for arquivo in request.arquivos_gerados or []
                 ],
             },
         },
         process_instance_id=request.id_tarefa_cliente,
     )
 
-    response = httpx.post(
-        f"{settings.CAMUNDA_ENGINE_URL}/message",
-        json=camunda_request.model_dump(by_alias=True),
-    )
-    response.raise_for_status()
-
-    db_session.add(
-        RPAEventLog(
-            process_id=request.id_tarefa_cliente,
-            event_type=RPAEventTypes.FINISH,
-            event_source=RPASource.MELIUS,
-            event_data=rpa_event_logs[0].event_data,
+    try:
+        _make_camunda_request(
+            f"{settings.CAMUNDA_ENGINE_URL}/message",
+            camunda_request.model_dump(by_alias=True),
         )
-    )
+
+        db_session.add(
+            RPAEventLog(
+                process_id=request.id_tarefa_cliente,
+                event_type=RPAEventTypes.FINISH,
+                event_source=RPASource.MELIUS,
+                event_data=rpa_event_logs[0].event_data,
+            )
+        )
+    except Exception as e:
+        # TODO: Adicionar error handling e notificação no Datadog/Slack
+        logger.error(f"Error sending message to Camunda Melius Webhook: {e}")
 
     return {"message": "Webhook Melius processado com sucesso"}
