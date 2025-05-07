6f9fcd4f4bc177fe216d106d66702f0f3f6203e0
Rafael Nunes <rafael.nunes@bhub.ai>
2025-05-07 11:57:50 -0300

    Removendo validacao do numero de eventos

--- app/service/rpa/rpa_services.py
+++ app/service/rpa/rpa_services.py
@@ -75,13 +75,18 @@ def handle_webhook_request(request: MeliusWebhookRequest, db_session: DBSession)
     - Processa o payload
     - Envia o payload para o Camunda
     """
-    stmt = select(RPAEventLog).where(
-        RPAEventLog.process_id == request.id_tarefa_cliente,
-        RPAEventLog.event_data.op("->>")("tokenRetorno") == request.token_retorno,  # type: ignore
+    stmt = (
+        select(RPAEventLog)
+        .where(
+            RPAEventLog.process_id == request.id_tarefa_cliente,
+            RPAEventLog.event_data.op("->>")("tokenRetorno") == request.token_retorno,  # type: ignore
+        )
+        .order_by(RPAEventLog.created_at.desc())
     )
+
     rpa_event_logs = db_session.execute(stmt).scalars().all()
 
-    if len(rpa_event_logs) != 1 or rpa_event_logs[0].event_type != RPAEventTypes.START:
+    if rpa_event_logs[0].event_type != RPAEventTypes.START:
         raise RPAException("Token inválido ou tarefa não encontrada")
 
     logger.info(f"Received Melius Webhook request with process_data: {request.model_dump()}")
